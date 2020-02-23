5c32e4669c35caabde08b5ba1d73e252e86218f4
liangchangyou <liangchangyoujackson@gmail.com>
2020-02-23 22:14:38 +0800

    rdd source

---
+++ src/main/scala/org/youdi/rdd/Demo.scala
@@ -0,0 +1,58 @@
+package org.youdi.rdd
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * RDD的创建
+ * 1. 从集合中创建
+ * 2. 从外部存储中
+ * 3. 其他RDD转换的
+ */
+
+object Demo {
+  def main(args: Array[String]): Unit = {
+    // 创建RDD
+    // 1) 从内存中创建
+    val conf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("makeRdd")
+    // 上下文
+    var sc: SparkContext = new SparkContext(conf)
+
+    // 从内存中创建
+    // makeRDD 并行度
+    /**
+     * override def defaultParallelism(): Int = {
+     *     conf.getInt("spark.default.parallelism", math.max(totalCoreCount.get(), 2))
+     * }
+     *
+     * totalCoreCount
+     *
+     * 自定义分区
+     */
+    val listRdd: RDD[Int] = sc.makeRDD(List(1, 2, 3, 4, 5, 6, 7, 8, 9), 2)
+
+    listRdd.collect().foreach(println)
+    val value: RDD[Int] = sc.parallelize(Array(1, 2, 3, 4, 5, 6))
+
+    // 从外部存储中获取
+    // 默认情况下可以使用项目路径， 也可以使用其他路径
+    /**
+     * def textFile(
+     * path: String,
+     * minPartitions: Int = defaultMinPartitions): RDD[String] = withScope {
+     * assertNotStopped()
+     * hadoopFile(path, classOf[TextInputFormat], classOf[LongWritable], classOf[Text],
+     * minPartitions).map(pair => pair._2.toString).setName(path)
+     * }
+     *
+     * 使用 hadoop 文件分区的概念
+     * def defaultMinPartitions: Int = math.min(defaultParallelism, 2)
+     */
+    val fileRdd: RDD[String] = sc.textFile("/Users/youdi/Project/javaProject/sparkOne/input")
+    fileRdd.collect().foreach(print)
+
+    // 将rdd保存到文件中
+    listRdd.saveAsTextFile("out")
+
+  }
+}
