4109fec4602b6e9bca7f0bb4e558312c8b728f3e
liangchangyou <liangchangyoujackson@gmail.com>
2020-02-24 21:11:26 +0800

    driver

---
+++ src/main/scala/org/youdi/rdd/Driver.scala
@@ -0,0 +1,25 @@
+package org.youdi.rdd
+
+/**
+ * driver：
+ * 创建spark上下文对象的应用程序称为driver
+ *
+ *
+ * 发送任务
+ *
+ *
+ * Execute:
+ * 执行器用于接收任务并执行任务
+ * 任务执行情况反馈
+ *
+ * 所有driver中的RDD计算功能都是excute进行的
+ *
+ * 序列化
+ *
+ * driver和execute上执行 进行
+ */
+object Driver {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
